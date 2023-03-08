b9e60394df6c22afa4762be63757a7ca8510c895
mikafeng <mikafengler@gmail.com>
2023-03-08 15:41:21 -0500

    changed api call for just the city, issues with taking lat lon valued and plugging in to forecast api

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const weatherAPIkey = "c12c4ce8b0b76d9ef75f2410f8b28128";
 const submitButton = document.getElementById('submitBtn')
-let city;
+let city = 'Atlanta';
 let state;
 let country;
 let limit;
@@ -16,29 +16,44 @@ var buttonClickHandler = function(event) {
 
 //use Geocoding to transform city, state, country to lat, long coordinates for 5weather api call forecast
 function getAPI () {
-    city = 'Atlanta',
-    state = 'Georgia',
-    country = 'USA',
-    limit = 5
-
-let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=${limit}&appid=${weatherAPIkey}`
-
-fetch(queryURL)
-    .then(response => response.json())
-    .then(data => {
-        for (var i=0;i<data.length; i++) {
-            console.log(data[i].lat);
-            console.log(data[i].lon);
-        }
-    });
-};
     
+    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIkey}`
 
+    fetch(queryURL)
+        .then(response => response.json())
+            
+        .then(data => {
+          console.log(data);
+           
+       });
+};
+
+// let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=${limit}&appid=${weatherAPIkey}`
+
+// fetch(queryURL)
+//     .then(response => response.json())
+//     .then(data => {
+//         for (var i = 0; i < data.length; i++) {
+//         let lat = data[i].lat;
+//         let lon = data[i].lon;
+//         console.log(lat, lon);
+//         }
+//         // for (var i=0;i<data.length; i++) {
+//         //     console.log(data[i].lat);
+//         //     console.log(data[i].lon);
+//         //     var latitude=data[i].lat;
+//         //     console.log(latitude);
+//         // }
+      
+//     });
+// };
+
+// getParams();
 
 //create user input for city, state, country
 
 
 //use response data for weather dashboard
-
+// function displayWeather();
 
 submitButton.addEventListener('click', buttonClickHandler);
