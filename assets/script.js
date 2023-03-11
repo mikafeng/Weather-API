24c1aee4b9131351dbfa255e9c23a9effb2c214f
mikafeng <mikafengler@gmail.com>
2023-03-10 22:47:22 -0500

    added async getForecast function for fetching weather api

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,48 @@
 const weatherAPIkey = "c12c4ce8b0b76d9ef75f2410f8b28128";
-const submitButton = document.getElementById('submitBtn')
-let city = 'Atlanta';
-let state;
-let country;
-let limit;
+const submitButton = document.getElementById('submitBtn');
+let city;
+let units = 'imperial';
 // let forecastURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIkey}`
 
 
 //Submit Button click event
 var buttonClickHandler = function(event) {
     event.preventDefault();
+    getForecast();
     console.log('I been clicked!')
 };
 
 
 //use Geocoding to transform city, state, country to lat, long coordinates for 5weather api call forecast
-function getAPI () {
-    
-    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherAPIkey}`
+async function getForecast () {
+    let city = document.getElementById("city-search").value;
+    let queryUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${weatherAPIkey}`;
+
+    await fetch(queryUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+        });
+};
+
+function getCurrentWeather () {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIkey}`
 
     fetch(queryURL)
         .then(response => response.json())
-            
-        .then(data => {
-          console.log(data);
+
+        .then(weather => {
+            console.log(weather)
            
-       });
+            
+
+        });
 };
 
-// let queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=${limit}&appid=${weatherAPIkey}`
 
-// fetch(queryURL)
-//     .then(response => response.json())
-//     .then(data => {
-//         for (var i = 0; i < data.length; i++) {
-//         let lat = data[i].lat;
-//         let lon = data[i].lon;
-//         console.log(lat, lon);
-//         }
-//         // for (var i=0;i<data.length; i++) {
-//         //     console.log(data[i].lat);
-//         //     console.log(data[i].lon);
-//         //     var latitude=data[i].lat;
-//         //     console.log(latitude);
-//         // }
-      
-//     });
-// };
 
 // getParams();
 
@@ -57,3 +53,5 @@ function getAPI () {
 // function displayWeather();
 
 submitButton.addEventListener('click', buttonClickHandler);
+
+
