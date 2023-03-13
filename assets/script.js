0c57fa2f7c995968c939fc2b3a82337d1dd857de
mikafeng <mikafengler@gmail.com>
2023-03-13 14:45:35 -0400

    add saveSearch and renderSearchHistory functions to store city array to localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,37 @@
+//Open Weather API key
 const weatherAPIkey = "c12c4ce8b0b76d9ef75f2410f8b28128";
+//global vars
 const submitButton = document.getElementById('submitBtn');
+const searchHistory = document.getElementById('lsOutput');
 let city;
 let units = 'imperial';
-// let forecastURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIkey}`
 
+let cityHistory = JSON.parse(localStorage.getItem("search")) || [];
 
 //Submit Button click event
 var buttonClickHandler = function(event) {
     event.preventDefault();
-    getForecast();
     getWeather();
-    console.log('I been clicked!')
+    getForecast();
+    saveSearch();
 };
 
+function saveSearch(){
+    let city = document.getElementById("city-search").value;
+    cityHistory.push(city);
+    localStorage.setItem("search",JSON.stringify(cityHistory));
+    renderSearchHistory();
+console.log(city)
+    
+};
+function renderSearchHistory(){
+    searchHistory.innerHTML="";
+    for (let i=0; i<cityHistory.length; i++) {
+        console.log(cityHistory);
+    }
+};
 
+//Use current weather API to pull data for current weather of input city
 function getWeather () {
     let city = document.getElementById("city-search").value;
     let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${weatherAPIkey}`
@@ -28,10 +46,18 @@ function getWeather () {
             // console.log(data)
         })
         .catch(console.error);
+        
+        // console.log(city);
+
+    // for (let i=0; i<localStorage.length; i++) {
+    //     const value = localStorage.getItem(city);
+    //     lsOutput.innerHTML += `${value}`;
+    // }
+
 };
 
 
-//use Geocoding to transform city, state, country to lat, long coordinates for 5weather api call forecast
+//Use 5day 3Hr API to pull data for 5day forecast of input city
 function getForecast () {
     let city = document.getElementById("city-search").value;
     let queryUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${weatherAPIkey}`;
@@ -49,19 +75,25 @@ function getForecast () {
         .catch(console.error);
 };
 
+//Display current forecast from getWeather function
 function displayToday(response){
     console.log(response);
     let row = document.querySelector('.currentWeather');
+    let dt = new Date (response.dt*1000);
     row.innerHTML = 
-    `
-    <h2>Today's Weather: ${response.weather[0].main}</h2>
-    <p class="card-text">High: ${response.main.temp_max} Low: ${response.main.temp_min}</p>
-    <p class="card-text">Wind Speed: ${response.wind.speed}</p>                        
-    <p class="card-text">Humidity: ${response.main.humidity}</p>
-    `
+    `<div class="container">
+    <h2>${dt.toDateString()}</h2>
+    <div class="row">
+    <h3 >Current Conditions: ${response.weather[0].main}</h3>
+    <img src="https://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt="${response.weather[0].description}"/>
+    </div>
+    <p>High: ${response.main.temp_max} Low: ${response.main.temp_min}</p>
+    <p>Wind Speed: ${response.wind.speed}</p>                        
+    <p>Humidity: ${response.main.humidity}</p>
+    </div>`
 };
 
-
+//Display 5 day forecast from the getForecast function
 function displayWeather(response){
      console.log(response);
     let row = document.querySelector('.weatherMain');
@@ -90,11 +122,8 @@ function displayWeather(response){
 };
 
 
-//create user input for city, state, country
 
 
-//use response data for weather dashboard
-// function displayWeather();
 
 submitButton.addEventListener('click', buttonClickHandler);
 
